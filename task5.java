60c58ec8ac8f4917ed5c1e5805d33b6f3d32d853
DmitriyTsi <98711244+DmitriyTsi@users.noreply.github.com>
2022-01-30 22:05:00 +0200

    task5 Hillel

---
+++ task5.java
@@ -0,0 +1,12 @@
+package Hillel1;
+
+public class task5 {
+    public static void main(String[] args) {
+
+        for(int i=1; i<30; i++) {
+            System.out.print("("+ i +")" + " ");
+
+        }
+
+    }
+}
