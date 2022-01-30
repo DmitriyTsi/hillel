122ec8682e059968889523df458c37a6965b11d4
DmitriyTsi <98711244+DmitriyTsi@users.noreply.github.com>
2022-01-30 22:04:31 +0200

    task4 Hillel

---
+++ task4.java
@@ -0,0 +1,32 @@
+package Hillel1;
+
+public class task4 {
+
+    public static void main(String[] args) {
+
+      String replace; {
+        String str = "abc";
+
+
+            System.out.println(str.charAt(0) +""+ str.charAt(1) +""+ str.charAt(2));
+            System.out.println(str.charAt(0) +""+ str.charAt(2) +""+ str.charAt(1));
+
+            System.out.println(str.charAt(1) +""+ str.charAt(0) +""+ str.charAt(2));
+            System.out.println(str.charAt(1) +""+ str.charAt(2) +""+ str.charAt(0));
+
+            System.out.println(str.charAt(2) +""+ str.charAt(0) +""+ str.charAt(1));
+            System.out.println(str.charAt(2) +""+ str.charAt(1) +""+ str.charAt(0));
+
+
+        }
+
+
+        }
+
+
+
+
+
+
+
+    }
